77b704739dfdf75ba1fd067aef956a8b78195038
Charles de Freitas <charles@defreitas.io>
2020-02-23 23:16:18 +0000

    Add getting module items
    Currently not passing meta to the parse_module_items callback

--- CanvasCURL/spiders/canvas.py
+++ CanvasCURL/spiders/canvas.py
@@ -2,13 +2,15 @@ import codecs
 import json
 import os
 import re
+from pathlib import Path
 
+import requests
 import scrapy
 
 
 reader = codecs.getreader("utf-8")
 
-class QuotesSpider(scrapy.Spider):
+class CanvasSpider(scrapy.Spider):
     name = "canvas"
     token = os.environ['TOKEN']
     headers = {"Authorization": f" Bearer {token}"}
@@ -25,17 +27,32 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         yield from self.page_response(response, self.parse)
-        page = response.url.split("/")[-2]
         j = json.loads(response.body_as_unicode())
         for course in j:
-            yield self.build_request(context=f"courses/{course['id']}/modules", callback=self.parse_course_modules)
-
+            yield self.build_request(context=f"courses/{course['id']}/modules", callback=self.parse_course_modules,
+                                     meta={"course_name": course['name']})
 
     def parse_course_modules(self, response):
         base = response.url.split("?")[0]
+        course = base.split("/")[6]
         j = json.loads(response.body_as_unicode())
         for i in j:
-            print(f"{base}/{i['id']}/items")
+            meta = {
+                "module_name": i['name'],
+                "course_name": response.meta['course_name']
+            }
+            yield self.build_request(context=f"{course}/{i['id']}/items", callback=self.parse_module_items, meta=meta)
+        yield from self.page_response(response, self.parse_module_items)
+
+    def parse_module_items(self, response):
+        pass
+
+    def download(self, response):
+        context = response.meta['path'] if response.meta['path'] else 'unknown_folder'
+        path = Path(self.output_dir) / context
+        path.mkdir(parents=True, exist_ok=True)
+        r = requests.get(json.loads(response)['url'])
+        print(r.headers['content-disposition'])
 
     def page_response(self, response, callback):
         if response.headers[b'Link']:
@@ -47,8 +64,6 @@ class QuotesSpider(scrapy.Spider):
 
     # Optionally set the url argument to call that URL with `start_page` parameter instead
     def build_request(self, context, callback, meta=None, url=None):
-        if url:
-            return scrapy.Request(url=f"{url}?{self.start_page}", callback=callback, headers=self.headers, meta=meta)
-        else:
-            return scrapy.Request(url=f"{self.base_url}/{context}?{self.start_page}", callback=callback,
-                                  headers=self.headers, meta=meta)
+        if not url:
+            url = f"{self.base_url}/{context}?{self.start_page}"
+        return scrapy.Request(url=f"{url}", callback=callback, headers=self.headers, meta=meta)
