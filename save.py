247a8f6f92131bbe7a567d6e0154e3bfd2b7174e
Charles de Freitas <me@chasbob.dev>
2020-10-26 11:38:54 +0000

    Add file size limit (defaults to about 150 mb)

--- save.py
+++ save.py
@@ -6,45 +6,48 @@ import requests
 import fire
 
 
-def save(item, prefix):
+def save(item, prefix, max_file_size):
     try:
         path = prefix / Path(item['path'])
         url = item['url']
         path.mkdir(parents=True, exist_ok=True)
         file_name = item['display_name']
-        file = path / file_name
+        file: Path = path / file_name
         if not file.is_file():
             try:
-                with open(path / file_name, 'wb') as file:
-                    download(file, file_name, url)
+                download(path, file_name, url, max_file_size)
             except IOError:
                 if not (path / item['filename']).is_file():
-                    with open(path / item['filename'], 'wb') as file:
-                        download(file, item['filename'], url)
+                    download(path, item['filename'], url, max_file_size)
         else:
             print(f"{file} exists!")
     except Exception as exception:
         print(exception)
 
 
-def download(file, file_name, url):
-    print("Downloading %s" % file_name)
-    response = requests.get(url, stream=True)
+def download(path, file_name, url, max_file_size):
+    response: requests.Response = requests.get(url, stream=True)
     total = response.headers.get('content-length')
 
-    if total is None:
-        file.write(response.content)
-    else:
-        downloaded = 0
-        total = int(total)
-        for data in response.iter_content(
-                chunk_size=max(int(total / 1000), 1024 * 1024)):
-            downloaded += len(data)
-            file.write(data)
-            done = int(50 * downloaded / total)
-            sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50 - done)))
-            sys.stdout.flush()
-    sys.stdout.write('\n')
+    if total > max_file_size:
+        print(f'File too large!\tSkipping...')
+        return
+    print(f'Downloading {file_name}')
+    with open(path / file_name, 'wb') as file:
+        if total is None:
+            file.write(response.content)
+        else:
+            downloaded = 0
+            total = int(total)
+            for data in response.iter_content(
+                    chunk_size=max(int(total / 1000), 1024 * 1024)):
+                downloaded += len(data)
+                file.write(data)
+                done = int(50 * downloaded / total)
+                sys.stdout.write('\r[{}{}]'.format('█' * done,
+                                                   '.' * (50 - done)))
+                sys.stdout.flush()
+        sys.stdout.write('\n')
 
 
 def gen_items(file: str):
@@ -53,9 +56,10 @@ def gen_items(file: str):
         for i in items:
             yield i
 
-def main(items, prefix):
+
+def main(items, prefix, max_file_size=154157394):
     for item in gen_items(items):
-        save(item, prefix)
+        save(item, prefix, max_file_size)
 
 
 if __name__ == "__main__":
